039f3c6d115238ec5b085bf7712cea60b0da788b
Thomas Braud <thomas.braud04@gmail.com>
2026-02-12 16:12:50 +0100

    Version SQL faite

--- src/dao/sequelize/article-model.js
+++ src/dao/sequelize/article-model.js
@@ -3,13 +3,30 @@ const sequelize = require('./database')
 
 const Article = sequelize.define('Article', {
     uid: {
-        type: DataTypes.STRING,
-        primaryKey: true
+        type: DataTypes.STRING(36),  // UUID fait 36 caractères
+        primaryKey: true,
+        allowNull: false
     },
     title: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    author: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    desc: {
+        type: DataTypes.TEXT,  // TEXT pour des descriptions longues
+        allowNull: false
+    },
+    ImgPath: {
+        type: DataTypes.STRING(500),  // URL peut être longue
+        allowNull: true,
+        defaultValue: 'https://m.media-amazon.com/images/I/71-vL7rKtDL._AC_UF1000,1000_QL80_.jpg'
     }
+}, {
+    tableName: 'articles',  // Nom de la table en BDD
+    timestamps: true  // Ajoute createdAt et updatedAt automatiquement
 })
 
 module.exports = Article
--- src/dao/sequelize/daoarticle-sequelize.js
+++ src/dao/sequelize/daoarticle-sequelize.js
@@ -2,10 +2,8 @@ const Article = require("./article-model");
 const IDAOArticle = require("../idaoarticle");
 
 class DAOArticleSequelize extends IDAOArticle {
-    /**
-     * Override explicitement si la methode existe dans le parent
-     */
     async insert(article) {
+        // Créer et sauvegarder en une seule fois
         return await Article.create(article);
     }
 
@@ -15,6 +13,37 @@ class DAOArticleSequelize extends IDAOArticle {
     async selectAll() {
         return await Article.findAll();
     }
+
+    async selectByUid(uid) {
+        return await Article.findOne({ where: { uid: uid } });
+    }
+
+    async deleteByUid(uid) {
+        const article = await Article.findByPk(uid);
+
+        if (!article) {
+            return null;  // Article non trouvé
+        }
+
+        await article.destroy();
+        return article;  // Retourne l'article supprimé
+    }
+
+    async updateArticle (uid, articleData) {
+        let article = await Article.findByPk(uid);
+
+        if (article) {
+            // Update
+            await article.update(articleData);
+        } else {
+            // Create
+            article = await Article.create({ uid, ...articleData });
+        }
+
+        return article;
+    }
+
+
 }
 
 module.exports = DAOArticleSequelize;
